4b63bb97760838fb66d47487eb796fb1ef7d9685
ecahyo <ecahyono507@gmail.com>
2023-12-23 11:59:52 +0700

    init

--- Base/Administrator/MasterData/Matauang.py
+++ Base/Administrator/MasterData/Matauang.py
@@ -1,21 +1,15 @@
 from seleniumbase import BaseCase
-import pandas as pd
+from os import environ, path
 import time
 import sys
-from faker import Faker
-sys.path.append('C:/Users/user/Documents/TRCH/KPK/eLkhpn')
+
+sys.path.append(environ.get('PYTHONPATH'))
+
 from config.userlogin import user
-fake = Faker('id_ID')
-textfield = fake.text()[:50]
+from config.readdataexternal import readfile
+from config.faker import *
 
-file_input = "input[type=file]" 
-file_path = "C:/Users/user/Documents/TRCH/KPK/eLkhpn/assets/image/barcode.jpg" 
-
-class MyTestClass(user):
-  def read_data_from_excel(self, excel_path, sheet_name):
-    # Read data from Excel using pandas
-    df = pd.read_excel(excel_path, sheet_name=sheet_name)
-    return df
+class MyTestClass(user, readfile ):
   def test_fill_form_from_excel(self):
     self.loginapp("test-user", "martanegara")
     time.sleep(3)
---
+++ config/readdataexternal.py
@@ -0,0 +1,9 @@
+from seleniumbase import BaseCase
+import time
+import pandas as pd
+
+class readfile(BaseCase):
+  def read_data_from_excel(self, excel_path, sheet_name):
+    # Read data from Excel using pandas
+    df = pd.read_excel(excel_path, sheet_name=sheet_name)
+    return df
